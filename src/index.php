5e2fea0e02be520d38dd44e2122c82115d3dc7fd
shingo-suzumura <shingo.msdf@gmail.com>
2021-05-04 20:42:51 +0900

    ディレクトリ作成

--- src/index.php
+++ src/index.php
@@ -1,38 +1,28 @@
 <?php
 
-class Post{
-    private $DB_HOST = "192.168.33.10";
-    private $DB_NAME = "practice";
-    private $DB_USER = "root";
-    private $DB_PASSWORD = "Shingo@9616";
-
-    protected function db_access(){
-        error_reporting(E_ALL & ~E_NOTICE);
-
-        try {
-            $dbh = new PDO('mysql:host='.$this->DB_HOST.';dbname='.$this->DB_NAME, $this->DB_USER, $this->DB_PASSWORD);
-            return $dbh;
-        } catch (PDOException $e) {
-            echo "エラー!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-    }
-
-    public function index(){
-        $dbh = $this->db_access();
-
-        $sql = 'SELECT * FROM posts';
-        $stmt = $dbh->prepare($sql);
-
-        $stmt->execute();
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $result; 
-    }
-
-}
 
 $postmodel = new Post();
 $result =$postmodel->index();
+$posts = $result;
 
-var_dump($result);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($posts as $post):?>
+    <div>
+        <h1><?php echo $post["title"];?></h1>
+    </div>
+    <div>
+        <p><?php echo $post["body"];?></p>
+    </div>
+    <?php endforeach; ?>
+</body>
+</html>

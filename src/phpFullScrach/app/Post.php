7a3fc7fb8c01caf8c9a867b569510d7c17faace6
shingo-suzumura <shingo.msdf@gmail.com>
2021-05-07 22:09:22 +0900

    validation

--- src/phpFullScrach/app/Post.php
+++ src/phpFullScrach/app/Post.php
@@ -18,6 +18,21 @@ class Post{
         }
     }
 
+    protected function validation($date_title,$date_body){
+        $error = array();
+
+        if(empty($date_title) || ctype_space($date_title)){
+            $error[] = "タイトルを入力してください";
+        }
+        if(empty($date_body) || ctype_space($date_body)){
+            $error[] = "本文を入力してください";
+        }
+        if(strlen($date_title) > 140){
+            $error[] = "本文を140字以下にしてください";
+        }
+        return $error;
+    }
+
     public function index(){
         $dbh = $this->db_access();
 
@@ -30,25 +45,51 @@ class Post{
     }
 
     public function store(){
-        $dbh = $this->db_access();
+        $post = array(
+            "title" => $_POST['title'],
+            "body" => $_POST['body'],
+        );
 
-        try{
-            $dbh->beginTransaction();
-            $sql = 'INSERT INTO posts(title,body) VALUES(:title, :body)';
-            $stmt = $dbh->prepare($sql);
-            $stmt->bindValue(':title',$_POST['title'],PDO::PARAM_STR);
-            $stmt->bindValue(':body',$_POST['body'],PDO::PARAM_STR);
-            $stmt->execute();
+        $error = $this->validation($post['title'],$post['body']);
 
-            $dbh->commit();
-        }catch(PDOException $Exception){
-            $dbh->rollback();
+        if(count($error)){
+
+        }else{
+            $dbh = $this->db_access();
+
+            try{
+                $dbh->beginTransaction();
+                $sql = 'INSERT INTO posts(title,body) VALUES(:title, :body)';
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindValue(':title',$post['title'],PDO::PARAM_STR);
+                $stmt->bindValue(':body',$post['body'],PDO::PARAM_STR);
+                $stmt->execute();
+
+                $dbh->commit();
+            }catch(PDOException $Exception){
+                $dbh->rollback();
+            }
         }
-        $result = array($_POST['title'],$_POST['body']);
-        return $result; 
+            $result = array($post,$error);
+            
+            return $result; 
     }
 
     public function show($article_id){
+
+        $dbh = $this->db_access();
+
+        $sql = 'SELECT * FROM posts WHERE id= :id';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':id',$article_id,PDO::PARAM_INT);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result; 
+    }
+
+    public function edit($article_id){
 
         $dbh = $this->db_access();
 
@@ -62,4 +103,37 @@ class Post{
         return $result; 
     }
 
+    public function update($article_id){
+        $dbh = $this->db_access();
+
+        try{
+            $dbh->beginTransaction();
+
+            $sql = 'UPDATE posts SET title= :title, body = :body WHERE id = :id';
+            $stmt = $dbh->prepare($sql);
+            $stmt->bindValue(':title',$_POST['title'],PDO::PARAM_STR);
+            $stmt->bindValue(':body',$_POST['body'],PDO::PARAM_STR);
+            $stmt->bindValue(':id',$article_id,PDO::PARAM_INT);
+            $stmt->execute();
+
+            $dbh->commit();
+        }catch(PDOException $Exception){
+            $dbh->rollback();
+        }
+        $result = array($_POST['title'],$_POST['body']);
+        return $result; 
+    }
+
+    public function destroy($article_id){
+
+        $dbh = $this->db_access();
+
+        $sql = 'DELETE FROM posts WHERE id = :id';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':id',$article_id,PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return "ok";
+    }    
 }
